06d6897dd081e5ec77de040224663db11be2c8e1
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-08-01 21:34:37 +0200

    add modal

---
+++ src/main/kotlin/com/example/springkotlindemo/modal/Person.kt
@@ -0,0 +1,12 @@
+package com.example.springkotlindemo.modal
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Person(@Id @GeneratedValue var id:Int? = null,
+                  @Column var firstname: String,
+                  @Column var email: String,
+                  @Column var age: Number )
