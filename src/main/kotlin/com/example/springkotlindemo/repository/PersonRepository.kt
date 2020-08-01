5219eaa632abf154f030f9fb0ae7ab712aa7fac2
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-08-01 21:35:00 +0200

    add person repository

---
+++ src/main/kotlin/com/example/springkotlindemo/repository/PersonRepository.kt
@@ -0,0 +1,9 @@
+package com.example.springkotlindemo.repository
+
+import com.example.springkotlindemo.modal.Person
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PersonRepository : JpaRepository<Person, Int> {
+}
